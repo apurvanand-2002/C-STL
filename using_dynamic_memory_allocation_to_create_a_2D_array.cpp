4568d6dd69913c27ed7299dc585f4ae9230a8e0f
Apurv Anand <93825178+apurvanand-2002@users.noreply.github.com>
2023-01-08 15:46:52 +0530

    Add files via upload

---
+++ using_dynamic_memory_allocation_to_create_a_2D_array.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+
+    int **arr = new int *[n];
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = new int[n];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << arr[i][j] << "\t";
+        }
+        cout << endl;
+    }
+}
